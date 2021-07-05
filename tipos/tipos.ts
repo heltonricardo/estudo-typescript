05403eb619a3f7a3b80210dcc82be45f909cacf1
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-05 12:05:27 -0300

    tipo never

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -110,3 +110,25 @@ nota = 10;
 console.log(`Minha nota é ${nota}`);
 nota = "10";
 console.log(`Minha nota é ${nota}`);
+
+/********************************* TIPO NEVER *********************************/
+
+// Para quando a função nunca retornará (loop infinito) ou sempre lança erro
+
+function falha(msg: string): never {
+  throw new Error(msg);
+}
+
+const produto = {
+  nome: "Sabão",
+  preco: 1,
+  validarProduto() {
+    if ((!this.nome || this.nome.trim().length === 0)) {
+      falha("Precisa ter um nome");
+    }
+    if (this.preco <= 0) {
+      falha("Preço inválido");
+    }
+  },
+};
+produto.validarProduto();
